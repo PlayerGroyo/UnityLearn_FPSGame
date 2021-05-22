62ed89af70603ae923cf421edeb5d365daf098ea
Groyo <554054729@qq.com>
2021-05-22 16:45:41 +0800

    adding projectile hit detection

--- FPSGame/Assets/Scripts/Weapon/ProjectileStandard.cs
+++ FPSGame/Assets/Scripts/Weapon/ProjectileStandard.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Weapon
 {
     public class ProjectileStandard : MonoBehaviour
     {
+        public Transform root;
+        public Transform tip;
+        public float radius = 0.01f;
+        public LayerMask hittableLayers = -1;
+        private Vector3 _spawnPoint;
+        
+        
         public float maxLifeTime = 5f;
         public float speed = 300f;
 
@@ -27,12 +33,58 @@ namespace Weapon
             
             // orient
             // transform.forward = _velocity.normalized;
+            
+            // hit detection
+            RaycastHit closestHit = new RaycastHit();
+            closestHit.distance = Mathf.Infinity;
+            bool foundHit = false;
+            
+            //SphereCastAll
+            Vector3 displacementSinceLastFrame = tip.position - _spawnPoint;
+            
+            RaycastHit[] hits = Physics.SphereCastAll(_spawnPoint, radius,
+                displacementSinceLastFrame.normalized,
+                displacementSinceLastFrame.magnitude,
+                hittableLayers,
+                QueryTriggerInteraction.Collide);
+
+            foreach (var hit in hits)
+            {
+                if (IsHitValid(hit) && hit.distance < closestHit.distance)
+                {
+                    closestHit = hit;
+                    foundHit = true;
+                }
+            }
+
+            if (foundHit)
+            {
+                if (closestHit.distance <= 0)
+                {
+                    closestHit.point = root.position;
+                    closestHit.normal = -transform.forward;
+                }
+
+                OnHit();
+            }
+        }
+
+        private void OnHit()
+        {
+            print("hit");
+            Destroy(gameObject);
+        }
+
+        private bool IsHitValid(RaycastHit hit)
+        {
+            return !hit.collider.isTrigger;
         }
 
         #endregion
 
         private void Shoot()
         {
+            _spawnPoint = root.position;
             _velocity = transform.forward * speed;
         }
     }
