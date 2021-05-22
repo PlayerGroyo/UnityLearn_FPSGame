0a3c3ecbee7eb080cafde39e083a37ee0a96587c
Groyo <554054729@qq.com>
2021-05-22 14:35:33 +0800

    adding WeaponController && PlayerWeaponManager

---
+++ FPSGame/Assets/Scripts/Weapon/PlayerWeaponManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Weapon
+{
+    /// <summary>
+    /// 负责管理玩家的所有武器，包括切换武器，添加武器等
+    /// </summary>
+    public class PlayerWeaponManager : MonoBehaviour
+    {
+        public List<WeaponController> startingWeapons = new List<WeaponController>();
+
+        public Transform weaponPosition;
+
+        public UnityAction<WeaponController> OnSwitchToWeapon;
+
+        private WeaponController[] _weaponSlots = new WeaponController[9];
+
+
+        #region Unity Event Functions
+
+        private void Start()
+        {
+            OnSwitchToWeapon += OnWeaponSwitched;
+
+            foreach (WeaponController weapon in startingWeapons)
+            {
+                AddWeapon(weapon);
+            }
+
+            SwitchWeapon();
+        }
+
+        
+
+        #endregion
+
+        #region weapon switch
+        
+        private bool AddWeapon(WeaponController weaponController)
+        {
+            for (int index = 0; index < _weaponSlots.Length; index++)
+            {
+                WeaponController weaponInstance = Instantiate(weaponController, weaponPosition);
+                weaponInstance.transform.localPosition = Vector3.zero;
+                weaponInstance.transform.localRotation = Quaternion.identity;
+
+                weaponInstance.Owner = gameObject;
+                weaponInstance.SourcePrefab = weaponController.gameObject;
+                weaponInstance.ShowWeapon(false);
+                _weaponSlots[index] = weaponInstance;
+
+                return true;
+            }
+
+            return false;
+        }
+        
+        private void SwitchWeapon()
+        {
+            SwitchWeaponToIndex(0);
+        }
+
+        private void SwitchWeaponToIndex(short weaponIndex)
+        {
+            if (weaponIndex < 0) { return; }
+
+            WeaponController newWeapon = GetWeaponAtSlot(weaponIndex);
+            OnSwitchToWeapon?.Invoke(newWeapon);
+        }
+
+        private WeaponController GetWeaponAtSlot(short weaponIndex)
+        {
+            return weaponIndex >= _weaponSlots.Length ? null : _weaponSlots[weaponIndex];
+        }
+
+        private void OnWeaponSwitched(WeaponController weaponController)
+        {
+            if (!weaponController)
+            {
+                return;
+            }
+            
+            weaponController.ShowWeapon(true);
+        }
+        
+
+        #endregion
+    }
+}
---
+++ FPSGame/Assets/Scripts/Weapon/WeaponController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Weapon
+{
+    /// <summary>
+    /// 负责控制玩家当前手中的武器
+    /// </summary>
+    public class WeaponController : MonoBehaviour
+    {
+        public GameObject weaponRoot;
+
+        public bool IsWeaponActive { get; private set; }
+
+        public GameObject Owner { get; set; }
+
+        public GameObject SourcePrefab { get; set; }
+
+        #region MyFunctions
+
+        /// <summary>
+        /// show or hide weapon
+        /// </summary>
+        /// <param name="active">true -> show || false -> hide</param>
+        public void ShowWeapon(bool active)
+        {
+            weaponRoot.SetActive(active);
+            IsWeaponActive = active;
+        }
+
+        #endregion
+    }
+}
